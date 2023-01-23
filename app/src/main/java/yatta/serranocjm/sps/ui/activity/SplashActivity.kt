d64a3d1df15bae640313a323215109bd10555d10
C. Serrano <serrano.cjm@gmail.com>
2023-01-23 15:06:46 -0400

    Activities...

--- app/src/main/java/yatta/serranocjm/sps/ui/activity/SplashActivity.kt
+++ app/src/main/java/yatta/serranocjm/sps/ui/activity/SplashActivity.kt
@@ -3,12 +3,16 @@ package yatta.serranocjm.sps.ui.activity
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import yatta.serranocjm.sps.R
+import yatta.serranocjm.sps.databinding.ActivitySplashBinding
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
